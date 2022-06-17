06747043abf59ebf881226f35dce763622e2b651
harutyunianlilit <harutyunianlilit@gmail.com>
2022-06-17 15:42:56 +0400

    first commit

--- src/Meme.js
+++ src/Meme.js
@@ -5,28 +5,41 @@ import memeData from "./memeData";
 export default function Meme () {
 
      //const [memeImage, setMemeImage] = React.useState(""); 
-    function GetMemeImage () {
-
-        const memesArrey = memeData.data.memes;
-        const randomNum = Math.floor(Math.random() * memesArrey.length)
-        setMemeImage (memesArrey[randomNum].url)
-    }
+    
 
     const [meme, setMeme] = React.useState ({
 
         topText: "",
         bottomText: '',
-        randomImage: ''
+        randomImage: 'https://i.imgflip.com/30b1gx.jpg'
     })
 
-    const [oldMemeImages, setOldMemeImages] = React.useState (memeData)
+    const [allMemeImages, setAllMemeImages] = React.useState (memeData)
+
+    function GetMemeImage () {
+
+        const memesArrey = allMemeImages.data.memes;
+        const randomNum = Math.floor(Math.random() * memesArrey.length)
+        const url = "memesArrey[randomNum].url"
+        setMeme (prevMeme => ({
+         ...prevMeme,
+         randomImage: url
+
+        }))
+    }
+    function hundleChange (event){
+ 
+        const {name, value} =event.target 
+        setMeme =(prevMeme => ({...prevMeme,
+        [name]: value}))
+    }
 return (
   <main>
     <div className="form">
-   <input type = 'text' className="form--input"/> 
-   <input type = 'text'className="form--input"/> 
+   <input type = 'text' className="form--input" name = 'topText' value = {meme.topText}/> 
+   <input type = 'text'className="form--input" name = 'bottomText' value = {meme.bottomText}/> 
    <button className="form--button" onClick = {GetMemeImage}>Get a new meme image</button>
-   <img src = {memeImage} className ='photo'/>
+   <img src = {meme.randomImage} className ='photo'/>
 </div>
 </main>
 
